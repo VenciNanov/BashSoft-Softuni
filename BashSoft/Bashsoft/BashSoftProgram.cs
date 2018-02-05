5d27d5d1d973a30bc18037d4f8b53e9917f925a8
VenciNanov <tep981@gmail.com>
2018-02-05 13:36:59 +0200

    Update

---
+++ BashSoft/Bashsoft/BashSoftProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bashsoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOManager.CreateDirectoryInCurrentFolder("pesho");
+
+            IOManager.TraverseDirectory(1);
+        }
+    }
+}
---
+++ BashSoft/Bashsoft/ExceptionMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bashsoft
+{
+    public static class ExceptionMessages
+    {
+        public const string ExampleExceptionMessage = "Example message!";
+
+        public const string DataAlreadyInitializedException = "Data  is already initialized!";
+
+        public const string DataNotInitializedExceptionMessage = "The data structure must be initialized first in order to make any operations with it.";
+
+        public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
+
+
+    }
+}
